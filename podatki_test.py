5f0fae6df2fe7d2c48db2e954cd91b770a26323a
unknown <maticcavalleri@gmail.com>
2020-04-01 22:26:15 +0200

    Kazni glede na proizvajalca avtomobila (absolutno)

--- podatki_test.py
+++ podatki_test.py
@@ -52,10 +52,11 @@ print(vehicle_colors)
 print(vehicle_makes)
 print(vehicle_body_types)
 
-# GRAF ŠTEVILA KAZNI PO DATUMIH:
-
 # branje in parsanje datumov v format datetime: 
 dataset = pd.read_csv("podatki/Parking_Violations_Issued_-_Fiscal_Year_2014__August_2013___June_2014__small.csv", parse_dates=['Issue Date'])
+
+# GRAF ŠTEVILA KAZNI PO DATUMIH:
+
 date_count = dataset.groupby(['Issue Date'])[['Issue Date']].agg('count')
 # dodajanje mankajočih datumov in jih fillat z ničlo:
 date_range = pd.date_range('03-01-2013', '09-01-2013')
@@ -68,7 +69,7 @@ plt.xticks(rotation=90)
 plt.ylabel('Št. parkirnih kazni')
 plt.show()
 
-# GRAF KAZNI GLEDE NA DAN V TEDNU:
+# KAZNI GLEDE NA DAN V TEDNU:
 
 weekday_count = dataset.groupby(dataset['Issue Date'].dt.dayofweek).agg('count')['Issue Date']
 # izris:
@@ -76,4 +77,16 @@ plt.bar(np.array(['mon','tue','wed','thu','fri','sat','sun']), weekday_count)
 plt.title("Kazni glede na dan v tednu 2013/2014", size=18)
 plt.xlabel('Dan v tednu')
 plt.ylabel('Skupno št. parkirnih kazni')
+plt.show()
+
+# KAZNI GLEDE NA PROIZVAJALCA AVTOMOBILA
+
+# dataset2 = pd.read_csv("podatki/Parking_Violations_Issued_-_Fiscal_Year_2014__August_2013___June_2014_.csv", parse_dates=['Issue Date'])
+vehicle_make_count = dataset.groupby(['Vehicle Make'])['Vehicle Make'].agg('count')
+# sortarinje padajoče:
+vehicle_make_count = vehicle_make_count.sort_values(ascending=False)
+vehicle_make_count.head(20).plot.barh()
+plt.title("Absolutno število kazni glede na proizvajalca avtomobila 2013/2014", size=18)
+plt.xlabel('Število kazni')
+plt.ylabel('Proizvajalec')
 plt.show()
